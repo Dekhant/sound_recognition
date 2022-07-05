5c6eb8b498b1858adfa371168c142390e013c9b5
ALex Dekhant <52196622+Dekhant@users.noreply.github.com>
2022-07-05 09:31:30 +0300

    sound recognition project

---
+++ android/app/src/main/kotlin/com/example/sounds_recognition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sounds_recognition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,216 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tflite_audio/tflite_audio.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final isRecording = ValueNotifier<bool>(false);
+  Stream<Map<dynamic, dynamic>>? result;
+
+  // final String model = 'assets/decoded_wav_model.tflite';
+  // final String label = 'assets/decoded_wav_label.txt';
+  // final String audioDirectory = 'assets/sample_audio_16k_mono.wav';
+  // final String inputType = 'decodedWav';
+  // final int sampleRate = 16000;
+  // final int bufferSize = 2000;
+  // // final int audioLength = 16000;
+
+  ///example values for google's teachable machine model
+  final String model = 'assets/soundclassifier_with_metadata.tflite';
+  final String label = 'assets/labels.txt';
+  final String inputType = 'rawAudio';
+  final String audioDirectory = 'assets/sample_audio_44k_mono.wav';
+  final int sampleRate = 44100;
+  final int bufferSize = 11016;
+
+  // final int audioLength = 44032;
+
+  final bool outputRawScores = false;
+  final int numOfInferences = 5;
+  final int numThreads = 1;
+  final bool isAsset = true;
+
+  final double detectionThreshold = 0.3;
+  final int averageWindowDuration = 1000;
+  final int minimumTimeBetweenSamples = 30;
+  final int suppressionTime = 1500;
+
+  @override
+  void initState() {
+    super.initState();
+    TfliteAudio.loadModel(
+      // numThreads: this.numThreads,
+      // isAsset: this.isAsset,
+      // outputRawScores: outputRawScores,
+      inputType: inputType,
+      model: model,
+      label: label,
+    );
+
+    //spectrogram parameters
+    // TfliteAudio.setSpectrogramParameters(nFFT: 256, hopLength: 129);
+
+    // mfcc parameters
+    TfliteAudio.setSpectrogramParameters(nMFCC: 40, hopLength: 16384);
+  }
+
+  void getResult() {
+    result = TfliteAudio.startAudioRecognition(
+      sampleRate: sampleRate,
+      bufferSize: bufferSize,
+      numOfInferences: numOfInferences,
+      // audioLength: audioLength,
+      // detectionThreshold: detectionThreshold,
+      // averageWindowDuration: averageWindowDuration,
+      // minimumTimeBetweenSamples: minimumTimeBetweenSamples,
+      // suppressionTime: suppressionTime,
+    );
+
+    result
+        ?.listen((event) =>
+            log("Recognition Result: " + event["recognitionResult"].toString()))
+        .onDone(() => isRecording.value = false);
+  }
+
+  Future<List<String>> fetchLabelList() async {
+    List<String> _labelList = [];
+    await rootBundle.loadString(label).then((q) {
+      for (String i in const LineSplitter().convert(q)) {
+        _labelList.add(i);
+      }
+    });
+    return _labelList;
+  }
+
+  String showResult(AsyncSnapshot snapshot, String key) =>
+      snapshot.hasData ? snapshot.data[key].toString() : '0 ';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(
+              title: const Text('Tflite-audio/speech'),
+            ),
+            body: StreamBuilder<Map<dynamic, dynamic>>(
+                stream: result,
+                builder: (BuildContext context,
+                    AsyncSnapshot<Map<dynamic, dynamic>> inferenceSnapshot) {
+                  return FutureBuilder(
+                      future: fetchLabelList(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<String>> labelSnapshot) {
+                        switch (inferenceSnapshot.connectionState) {
+                          case ConnectionState.none:
+                            if (labelSnapshot.hasData) {
+                              return labelListWidget(labelSnapshot.data);
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          case ConnectionState.waiting:
+                            return Stack(children: <Widget>[
+                              Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: inferenceTimeWidget('calculating..')),
+                              labelListWidget(labelSnapshot.data),
+                            ]);
+
+                          default:
+                            return Stack(children: <Widget>[
+                              Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: inferenceTimeWidget(showResult(
+                                          inferenceSnapshot, 'inferenceTime') +
+                                      'ms')),
+                              labelListWidget(
+                                  labelSnapshot.data,
+                                  showResult(
+                                      inferenceSnapshot, 'recognitionResult'))
+                            ]);
+                        }
+                      });
+                }),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: ValueListenableBuilder(
+                valueListenable: isRecording,
+                builder: (context, value, widget) {
+                  if (value == false) {
+                    return FloatingActionButton(
+                      onPressed: () {
+                        isRecording.value = true;
+                        setState(() {
+                          getResult();
+                        });
+                      },
+                      backgroundColor: Colors.blue,
+                      child: const Icon(Icons.mic),
+                    );
+                  } else {
+                    return FloatingActionButton(
+                      onPressed: () {
+                        log('Audio Recognition Stopped');
+                        TfliteAudio.stopAudioRecognition();
+                      },
+                      backgroundColor: Colors.red,
+                      child: const Icon(Icons.adjust),
+                    );
+                  }
+                })));
+  }
+
+  Widget labelListWidget(List<String>? labelList, [String? result]) {
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: labelList!.map((labels) {
+              if (labels == result) {
+                return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text(labels.toString(),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.green,
+                        )));
+              } else {
+                return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text(labels.toString(),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        )));
+              }
+            }).toList()));
+  }
+
+  Widget inferenceTimeWidget(String result) {
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(result,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.black,
+            )));
+  }
+}
